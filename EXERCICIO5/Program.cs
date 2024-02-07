e6ca3bfeb367315ffeed1f6a1d17eea4cf3fb8d7
Lilian Graciano <lilian.graciano123@outlook.com>
2024-02-07 20:00:44 -0300

    Subindo exercicio 5 para teste

---
+++ EXERCICIO5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EXERCICIO5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma string para inverter: \n");
+            string input = Console.ReadLine();
+
+            string invertedString = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                invertedString += input[i];
+            }
+
+            Console.WriteLine($"String invertida: {invertedString}");
+        }
+    }
+}
